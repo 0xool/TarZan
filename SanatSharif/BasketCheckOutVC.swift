b5114264a2efb13d8fa13c337408bd1d39f45d4a
cyrus refahi <cyrusrefahi@cyruss-MacBook-Pro-2.local>
2016-09-18 17:28:41 +0430

    A few twickes on project build settings added IOS 10 Capability

---
+++ SanatSharif/BasketCheckOutVC.swift
@@ -0,0 +1,26 @@
+//
+//  BasketCheckOutVC.swift
+//  SanatSharif
+//
+//  Created by cyrus refahi on 9/18/16.
+//  Copyright © 2016 cyrus refahi. All rights reserved.
+//
+
+import UIKit
+
+class BasketCheckOutVC: UIViewController {
+
+    private var _cart : Cart!
+    
+    
+    override func viewDidLoad() {
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
