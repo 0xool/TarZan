195901044633881f7ea1b4b1f78b4fe9ad5d2d46
cyrus refahi <cyrusrefahi@cyruss-MacBook-Pro-2.local>
2016-09-22 15:27:40 +0330

    a few bug fixes regarfin the plus button width and height

---
+++ SanatSharif/ProfileVCViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileVCViewController.swift
+//  SanatSharif
+//
+//  Created by cyrus refahi on 9/22/16.
+//  Copyright © 2016 cyrus refahi. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVCViewController: UIViewController {
+
+    @IBOutlet weak var image : UIImageView!
+    @IBOutlet weak var nameLabrl : UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
